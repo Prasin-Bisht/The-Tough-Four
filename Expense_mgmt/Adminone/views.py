128df53700a04b02ffd261c4232e881a7972d6a5
asra <asramansuri12@gmail.com>
2025-10-04 10:46:44 +0530

    views

--- Expense_mgmt/Adminone/views.py
+++ Expense_mgmt/Adminone/views.py
@@ -5,11 +5,7 @@ from .models import AccountModel
 from .forms import AccountForm,LoginForm
 # Create your views here.
 
-<<<<<<< HEAD
-def Login(request):
-=======
-def signup(request):
->>>>>>> f9a2a5c40ef255910d4057731274eceeccf524eb
+def login(request):
     if request.method == 'POST':
         form = AccountForm(request.POST)
         if form.is_valid():
